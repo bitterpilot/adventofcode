d88c40924cd0cb051a59de00aa2817b3f2232bca
Nathan Minissale <nathanminissale@gmail.com>
2019-12-03 09:55:43 +0800

    Part 2 tests are passing

--- 2019/day/1/fuel/rocket_equation.go
+++ 2019/day/1/fuel/rocket_equation.go
@@ -19,5 +19,15 @@ func CounterUpper(masses []float64) float64 {
 // Specifically, to find the fuel required for a module, take its mass,
 // divide by three, round down, and subtract 2.
 func moduleFuelRequirement(mass float64) float64 {
-	return math.Floor(mass/3) - 2
+	var fuel float64
+	fuel += math.Floor(mass/3) - 2
+	mass = fuel
+	if mass > 0 {
+		fuel += moduleFuelRequirement(mass)
+	}
+	if fuel < 0 {
+		fuel = 0
+	}
+
+	return fuel
 }
